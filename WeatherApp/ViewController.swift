929f9051af066c8a82a3912d4015eb246c4616f3
Halit Uğurgelen <halit_ugurgelen@hotmail.com>
2020-06-26 13:23:25 +0300

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Halit Uğurgelen on 26.06.2020.
+//  Copyright © 2020 Halit Uğurgelen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

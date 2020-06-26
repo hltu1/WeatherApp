08cb22bb2bdf91c3aa411530810d4b0f9f9e047b
Halit Uğurgelen <halit_ugurgelen@hotmail.com>
2020-06-26 21:47:51 +0300

    added some model classes (empty for now)

---
+++ WeatherApp/src/class/ThreeFieldVerticalContainer.swift
@@ -0,0 +1,9 @@
+//
+//  ThreeFieldVerticalContainer.swift
+//  WeatherApp
+//
+//  Created by Halit Uğurgelen on 26.06.2020.
+//  Copyright © 2020 Halit Uğurgelen. All rights reserved.
+//
+
+import Foundation

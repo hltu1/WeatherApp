c5010c88c88caddb07fd1fb54fd6f8adf5766132
Halit Uğurgelen <halit_ugurgelen@hotmail.com>
2020-06-27 16:51:11 +0300

    parameterizing gradually

--- WeatherApp/src/class/VC/ThreeFieldVerticalContainerVC.swift
+++ WeatherApp/src/class/VC/ThreeFieldVerticalContainerVC.swift
@@ -15,18 +15,63 @@ class ThreeFieldVerticalContainerVC: UIViewController {
     @IBOutlet weak var temperatureLabel: UILabel!
     
     @IBOutlet weak var hourBackground: UIView!
+    @IBOutlet      var selectedHourWeatherContainerView: UIView!
     
     // methods
     
+    enum ContainerType {
+        case defaultType
+        case selectedHour
+        case adjacentHour
+        case forecastDay
+    }
+    
+    var containerType: ContainerType = .defaultType
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        if hourBackground != nil {
-            hourBackground.layer.cornerRadius = 20
+        
+        switch self.title {
+            case "selectedHour":
+                self.containerType = .selectedHour
+            case "adjacentHour":
+                self.containerType = .adjacentHour
+            case "forecastDay":
+                self.containerType = .forecastDay
+            default:
+                break
         }
         
-        timeLabel.text = "09:00" // TODO: put day info in forecast container
+        // no need for default since all cases are handled
+        switch containerType {
+            case .defaultType:
+                break
+            case .selectedHour: do {
+                timeLabel.text = "09:00" // TODO: parameterize/handle
+                temperatureLabel.text = "19°"
+            }
+            case .adjacentHour: do {
+                timeLabel.text = "19:00" // TODO: parameterize/handle
+                temperatureLabel.text = "17°"
+            }
+            case .forecastDay: do {
+                timeLabel.text = "sun" // TODO: parameterize/handle
+                temperatureLabel.text = "15°"
+            }
+        }
+        
+        // decide if the view being handled is for current hour or adjacent hours
+        let isViewOfCurrentHour: Bool = hourBackground != nil
+
+        // Do any additional setup after loading the view.
+        if isViewOfCurrentHour {
+            hourBackground.layer.cornerRadius = 20
+            selectedHourWeatherContainerView.layer.cornerRadius = 40
+        }
+        
+        
+        
+        
     }
     
     /*

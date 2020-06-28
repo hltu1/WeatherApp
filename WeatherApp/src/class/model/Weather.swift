e457ed0d4966a4210d741144fd3e7ba617bdf065
Halit Uğurgelen <halit_ugurgelen@hotmail.com>
2020-06-28 14:17:16 +0300

    added dummy test value generators

--- WeatherApp/src/class/model/Weather.swift
+++ WeatherApp/src/class/model/Weather.swift
@@ -8,8 +8,8 @@
 
 import Foundation
 
-enum WeatherType {
-    case defaultWeather // used for initial state, checked for errors
+enum WeatherType: UInt {
+    case defaultWeather = 0 // used for initial state, checked for errors
     case sunny
     case cloudy
     case rainy
@@ -21,17 +21,17 @@ enum WeatherType {
 class Weather {
     var date       : Date
     var weather    : WeatherType
-    var temperature: UnitTemperature
+    var temperature: Int
     
     // default init.
     init() {
         date        = .init(timeIntervalSinceNow: .init(0))
         weather     = .defaultWeather // TODO: handle default case for errors
-        temperature = .init(symbol: "˚C") // Celsius degrees
+        temperature = 0
     }
     
     // assign-all init.
-    init(date: Date, weather: WeatherType, temperature: UnitTemperature) {
+    init(date: Date, weather: WeatherType, temperature: Int) {
         self.date        = date
         self.weather     = weather
         self.temperature = temperature
